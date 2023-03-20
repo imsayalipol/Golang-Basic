fcfee7a86f50c9b501daff78006b2662840ea23c
sayalipol <sayalip1506@gmail.com>
2023-03-20 18:16:08 +0530

    basics of Go

--- Basic/hello.go
+++ Basic/hello.go
@@ -1,6 +1,14 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+)
 
 func main() {
 	fmt.Println("Hello ,world !")
+	a := 10
+	fmt.Println("type: ", reflect.TypeOf(a))
+	fmt.Println("size: ", unsafe.Sizeof(a))
 }
