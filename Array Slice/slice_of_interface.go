6037c3a27d8d790e7cba6ac53ce79703cb258185
sayalipol <sayalip1506@gmail.com>
2023-03-21 10:01:16 +0530

    array and slice related

---
+++ Array Slice/slice_of_interface.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	name string
+	age  int
+}
+
+func main() {
+	var interfaceSlice []interface{}
+
+	interfaceSlice = append(interfaceSlice, 20, "Taylor", 10.10, true, nil)
+	fmt.Println(interfaceSlice)
+
+	//in case of fmt.Println, the ... notation is used to print the elements of a slice as separate values.
+	fmt.Println(interfaceSlice...)
+
+	// slice of struct
+	var structSlice []Person
+	structSlice = append(structSlice, Person{"James", 25})
+	structSlice = append(structSlice, Person{"MoneyPenny", 30})
+
+	fmt.Println(structSlice)
+}
