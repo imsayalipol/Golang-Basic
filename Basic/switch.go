50f951c4b0bbe2da86465ce37936ab20797b0c94
sayalipol <sayalip1506@gmail.com>
2023-03-23 03:03:29 +0530

    switch on golang

---
+++ Basic/switch.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// switch with switch statement and switch expressions
+	switch ch := "a"; ch {
+	case "a":
+		fmt.Println("Its A")
+
+	case "b":
+		fmt.Println("Its B")
+
+		//there can be multiple expressions in the case
+	case "c", "d":
+		fmt.Println("Its C or D")
+	}
+
+	//both switch statement and switch expression
+	age := 45
+
+	switch {
+	case age < 24:
+		fmt.Println("Teens")
+
+	case age > 25 && age < 50:
+		fmt.Println("Adults")
+
+	case age > 50:
+		fmt.Println("Old")
+	}
+
+	// Only switch statement
+	switch number := 33; {
+	case number < 20:
+		fmt.Println("Less than 20")
+
+	case number < 50:
+		fmt.Println("Less than 50")
+	}
+
+}
