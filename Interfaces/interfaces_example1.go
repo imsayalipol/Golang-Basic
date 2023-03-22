305c57a44f0dfdf8c4913816c2c7974848485f70
sayalipol <sayalip1506@gmail.com>
2023-03-22 20:21:34 +0530

    interface related

--- Interfaces/interfaces_example1.go
+++ Interfaces/interfaces_example1.go
@@ -1,28 +1,29 @@
 package main
+
 import "fmt"
 
-type Shape interface{
+type Shape interface {
 	Area() int
 	// Perimeter() int
 }
 
-type Rectangle struct{
-	width int
+type Rectangle struct {
+	width  int
 	height int
 }
 
 func (r Rectangle) Area() int {
-	return 2*r.width*r.height
+	return 2 * r.width * r.height
 }
 
 func geometry(s Shape) {
-	fmt.Println("s : ",s)
+	fmt.Println("s : ", s)
 	fmt.Println(s.Area())
 	// fmt.Println(s.Perimeter())
 }
 
-func main(){
-	r := Rectangle{width:10, height:5}
+func main() {
+	r := Rectangle{width: 10, height: 5}
 	geometry(r)
 
 }
---
+++ Interfaces/interfaces_function_as_type.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+type square struct {
+	width int
+}
+type rectangle struct {
+	width  int
+	length int
+}
+
+// circle implementing the interface
+func (s square) Area() int {
+	return s.width * s.width
+}
+func (s square) Perimeter() int {
+	return 2 * s.width
+}
+
+// rectangle implementing the interface
+func (r rectangle) Area() int {
+	return 2*r.length + 2*r.width
+}
+func (r rectangle) Perimeter() int {
+	return 2 * r.width * r.length
+}
+
+type shapes interface {
+	Area() int
+	Perimeter() int
+}
+
+func main() {
+	var s []shapes //interface variable
+
+	squ := &square{width: 10}
+	s = append(s, squ)
+	rec := rectangle{width: 5, length: 10}
+	s = append(s, rec)
+
+	for _, shape := range s {
+		fmt.Println("area: ", shape.Area(), "perimeter:", shape.Perimeter())
+	}
+}
