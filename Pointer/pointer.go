7f5915d69e85709a86b39e3eaaee209049628d98
sayalipol <sayalip1506@gmail.com>
2023-03-25 19:47:07 +0530

    golang Pointer

---
+++ Pointer/pointer.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	a := 10
+	b := new(int)
+
+	fmt.Println("Type of a :", reflect.TypeOf(a))
+	fmt.Println("Type of b :", reflect.TypeOf(b))
+	fmt.Println("a=", a, "b=", b, "*b=", *b)
+
+	b = &a
+	fmt.Println("*b = ", *b)
+}
