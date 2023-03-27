0f7b5ea7cded97ca685f88c4a8cb8382cec33b17
sayalipol <sayalip1506@gmail.com>
2023-03-27 09:56:21 +0530

    Marshaling and Unmarshaling

---
+++ Struct/print_struct_JSON_form.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+func main() {
+	type University struct {
+		Name         string
+		StudentCount int
+		Courses      []string
+	}
+
+	var BostonUniversity = University{
+		Name:         "Boston",
+		StudentCount: 500,
+		Courses:      []string{"AI", "ML", "Data Science", "Embedded Systems"},
+	}
+
+	// Marshalling
+	MarshalData, err := json.Marshal(BostonUniversity)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	fmt.Printf("Marshaled struct is : %s\n", MarshalData)
+
+	MarshalIndentData, err := json.MarshalIndent(BostonUniversity, "", " ")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	fmt.Printf("Marshal Indent is : %s", MarshalIndentData)
+
+	var newUni University
+
+	// Unmarshaling
+	err = json.Unmarshal(MarshalData, &newUni)
+	fmt.Println("newUni :", newUni)
+}
