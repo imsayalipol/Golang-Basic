7c52f48a5163e1ca3aa01e107d09407d2a9d6b66
sayalipol <sayalip1506@gmail.com>
2023-03-30 18:06:52 +0530

    interface embedding in struct and  interface

---
+++ Interfaces/12.interfaceEmbedding_struct.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+type animal interface {
+	walks()
+}
+
+type pet struct {
+	age int
+}
+
+func (p pet) walks() {
+	fmt.Println("This animal walks")
+}
+
+type dog struct {
+	a    animal // named interface
+	name string
+}
+
+type cat struct {
+	animal // embedded anonymously
+	name   string
+}
+
+func main() {
+
+	p := pet{age: 5}
+	var a animal
+	a = p
+
+	// if interface is named we call it with that name, here 'a'
+	d := dog{a: a, name: "Marcos"}
+	d.a.walks()
+	// d.walks()  this will throw error
+
+	// for anonymous interafce we call it with interface name
+	c := cat{animal: a, name: "Snowy"}
+	c.walks()
+
+}
+
+// important :
+// If we don’t initialise the embedded interface animal, then it will be intialised
+//with the zero value of the interface which is nil.
+//Calling  walks() method  on such an instance of dog or cat struct will
+//create a panic.
