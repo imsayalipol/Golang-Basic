c3e7fdda8f315d28891f7c759fd448baea5de32a
sayalipol <sayalip1506@gmail.com>
2023-03-31 15:20:00 +0530

    comments added

--- Channel/1.channel.go
+++ Channel/1.channel.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func main() {
-// empty channels
+	// empty channels
 
+	// channel declaration
 	ch1 := make(chan int)
 	fmt.Println("Channel 1:", ch1)
 
+	// channel definition with make()
 	var ch2 chan int
 	fmt.Println("Channel 2:", ch2)
 }
