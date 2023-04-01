b0c23c2b1c14a37e9f8fe3a1a7e2e0c0f82f6f01
sayalipol <sayalip1506@gmail.com>
2023-04-01 08:12:23 +0530

    close() operation on cHANNEL

--- Channel/5.send_receive_only.go
+++ Channel/5.send_receive_only.go
@@ -15,12 +15,18 @@ func main() {
 	chRec <- 10
 	receiving(chRec)
 
+	fmt.Println("Capacity of chSend", cap(chSend))
+	
+
 }
 
+// send only channel
 func sending(chSend chan<- int) {
 	chSend <- 2
+	fmt.Println("Length of chSend", len(chSend))
 }
 
+// receive only channel
 func receiving(chRec <-chan int) {
 	val := <-chRec
 	fmt.Println("chRec :", val)
---
+++ Channel/6.close_channel.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	ch := make(chan int, 3)
+
+	go add(ch, 3)
+
+	ch <- 2
+	ch <- 1
+	ch <- 2
+	close(ch)
+
+	time.Sleep(1 * time.Second)
+}
+
+func add(ch chan int, len int) {
+	add := 0
+	for i := 0; i < len; i++ {
+		add += <-ch
+	}
+	fmt.Println("sum :", add)
+}
