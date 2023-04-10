26de8ade805521aa29f533a4cab3e114a0fbb313
sayalipol <sayalip1506@gmail.com>
2023-04-10 08:52:00 +0530

    strings

--- Strings/range_string.go
+++ Strings/range_string.go
@@ -8,5 +8,15 @@ func main() {
 
 	for i, v := range name {
 		fmt.Printf("Index :%d Value :%s\n", i, string(v))
+		// fmt.Println("Index :", i, " Value", v)
+
+	}
+
+	var animal string
+	fmt.Println("animal :", animal)
+	// iterating empty string won't through error
+	for m, n := range animal {
+		fmt.Printf("Index :%d Value :%s\n", m, string(n))
+
 	}
 }
