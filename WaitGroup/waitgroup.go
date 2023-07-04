5018974463f647a597b285d434781b33ca3f41a9
sayalipol <sayalip1506@gmail.com>
2023-07-04 10:02:40 +0530

    WaitGroup

---
+++ WaitGroup/waitgroup.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func RunnerOne(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("Name :", s)
+}
+
+func RunnerTwo(age int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("Age :", age)
+}
+
+func main() {
+
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	go RunnerOne("John", &wg)
+	go RunnerTwo(90, &wg)
+
+	wg.Wait()
+
+	fmt.Println("We exit")
+
+	// wg.Wait()    // uncomment this, comment line 27 and run
+
+}
